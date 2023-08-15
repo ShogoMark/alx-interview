6e2076d3c4d9d992552769e58ddc4ab670f6e1a4
ShogoMark <trigmark87@gmail.com>
2023-08-15 04:32:20 +0100

    task00

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,23 +4,25 @@
 
 def canUnlockAll(boxes):
     """Method that determines if all the boxes can be opened"""
-    
-    matches = []
 
-    for i in range(len(boxes)):
-        current_box = boxes[i]
+    if type(boxes) != list or len(boxes) == 0:
+        return False
 
-        for j in range(len(current_box)):
-            current_item = current_box[j]
+    boxLength = len(boxes)
 
-            for k in range(i + 1, len(boxes)):
-                other_box = boxes[k]
+    freeKeys = [0]
+    usedKeys = set()
 
-                if i != k and current_item in other_box:
-                    matches.append(True)
-                    break
-            else:
-                continue
-            break
+    while freeKeys:
+        openedBoxIndex = freeKeys.pop()
+        usedKeys.add(openedBoxIndex)
 
-    return all(matches)
+        openedBox = boxes[openedBoxIndex]
+
+        if type(openedBox) != list:
+            return False
+
+        for key in openedBox:
+            if key < boxLength and key not in freeKeys and key not in usedKeys:
+                freeKeys.append(key)
+    return len(usedKeys) == boxLength
